134ee7af7f2a09bf28a2a46ddfed7d48c628172f
gautamshubham <shubham@engagedly.com>
2024-09-23 01:03:52 +0530

    [Atlys-Scraper] Commit Includes:
    1. Implemented the scrape API to scrape product data and store to database.
    2. Implemented the cache mechmism to retrive data.
    3. Implemented the auth mechnism to authenticate the request with `Bearer` token.

---
+++ config/db_config.py
@@ -0,0 +1,6 @@
+class Config:
+  BASE_URL = "https://dentalstall.com/shop"
+  MAX_PAGES = 5  # Default number of pages to scrape
+  RETRY_DELAY = 3  # Retry delay in seconds
+  CACHE_DB = "product_cache.db"
+  STATIC_TOKEN = "80bef3cb4339d58442fdb3959a43732e7d58680ee3b2a9bf847987886ee2cbd40a0a3ff58ffc3e16f0746b8e09c66e37a3cf0ca03ab530964e4d43e494434b8f"
